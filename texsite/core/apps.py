e7de3ef8fd503c8eba8a939d94ddef636cd58ec3
Timo Rieber <dev@timorieber.de>
2015-11-09 14:13:09 +0100

    Initialized core app

---
+++ texsite/core/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class TexsiteCoreAppConfig(AppConfig):
+    name = 'texsite.core'
+    label = 'texsitecore'
+    verbose_name = "texsite core"

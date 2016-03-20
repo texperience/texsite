73a804c7830a3b66c7dd841eaa20c2058f6843a7
Timo Rieber <trieber@texperience.de>
2016-03-20 09:56:20 +0100

    Upgraded to wagtail 1.4 and removed older versions

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
-import os
 from setuptools import setup, find_packages
 from texsite import __version__
 
-# Hard linking doesn't work inside VirtualBox shared folders. This means that you can't use tox in a directory that is
-# being shared with Vagrant, since tox relies on `python setup.py sdist` which uses hard links. As a workaround, disable
-# hard-linking if setup.py is a descendant of /vagrant. See
-# https://stackoverflow.com/questions/7719380/python-setup-py-sdist-error-operation-not-permitted for more details.
-if 'vagrant' in os.path.abspath(__file__).split(os.path.sep):
-    del os.link
-
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
-# Allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+# Installation dependencies
+install_requires = [
+    'django-bootstrap-ui>=0.2,<0.3',
+    'wagtail>=1.4,<1.5',
+]
 
 setup(
     name='texsite',
@@ -23,19 +15,15 @@ setup(
     license='ISC License (ISCL)',
     description='texsite is a modern web content management system. It is written in Python and built on Wagtail CMS, '
                 'which is backed by the Django web framework.',
-    long_description=README,
+    long_description=open('README.rst').read(),
     url='https://github.com/texperience/texsite',
     author='Timo Rieber',
     author_email='trieber@texperience.de',
-    install_requires=[
-        'django-bootstrap-ui>=0.1,<0.2',
-        'wagtail>=1.0,<1.4',
-    ],
+    install_requires=install_requires,
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Intended Audience :: Developers',
